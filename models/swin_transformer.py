f35770ee16d8a9a27539ab221a17576873a07b24
tarun-kumar-revelli <cs22b2028@iiitdm.ac.in>
2025-12-26 20:52:47 +0530

    Added dry run provision

--- models/swin_transformer.py
+++ models/swin_transformer.py
@@ -241,129 +241,14 @@ class SwinTransformerBlock(nn.Module):
         x = x + self.mlp(self.norm2(x))
         return x
 
-# class SwinTransformerSegmentation(nn.Module):
-#     """
-#     Swin Transformer-based segmentation model for MA-UDA
-#     """
-    
-#     def __init__(self, img_size=256, patch_size=4, in_chans=3, num_classes=4,
-#                  embed_dim=96, depths=[2, 2, 6, 2], num_heads=[3, 6, 12, 24],
-#                  use_checkpoint=True): # <--- Enabled by default
-#         super().__init__()
-        
-#         self.use_checkpoint = use_checkpoint
-#         self.num_classes = num_classes
-#         self.num_layers = len(depths)
-#         self.embed_dim = embed_dim
-#         self.num_features = int(embed_dim * 2 ** (self.num_layers - 1))
-        
-#         # Patch embedding
-#         self.patch_embed = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
-#         self.patches_resolution = self.patch_embed.patches_resolution
-        
-#         # Stages and Downsamplers
-#         self.stages = nn.ModuleList()
-#         self.downsamplers = nn.ModuleList()
-        
-#         for i_layer in range(self.num_layers):
-#             layer_dim = int(embed_dim * 2 ** i_layer)
-            
-#             stage_blocks = nn.ModuleList([
-#                 SwinTransformerBlock(
-#                     dim=layer_dim,
-#                     num_heads=num_heads[i_layer],
-#                     window_size=4
-#                 )
-#                 for _ in range(depths[i_layer])
-#             ])
-#             self.stages.append(stage_blocks)
-            
-#             if i_layer < self.num_layers - 1:
-#                 self.downsamplers.append(nn.Linear(layer_dim, layer_dim * 2))
-        
-#         # Decoder
-#         self.decoder = nn.ModuleList([
-#             nn.Sequential(
-#                 nn.ConvTranspose2d(self.num_features, self.num_features // 2, 2, stride=2),
-#                 nn.BatchNorm2d(self.num_features // 2),
-#                 nn.ReLU(inplace=True)
-#             ),
-#             nn.Sequential(
-#                 nn.ConvTranspose2d(self.num_features // 2, self.num_features // 4, 2, stride=2),
-#                 nn.BatchNorm2d(self.num_features // 4),
-#                 nn.ReLU(inplace=True)
-#             ),
-#             nn.Sequential(
-#                 nn.ConvTranspose2d(self.num_features // 4, self.num_features // 8, 2, stride=2),
-#                 nn.BatchNorm2d(self.num_features // 8),
-#                 nn.ReLU(inplace=True)
-#             )
-#         ])
-        
-#         self.segmentation_head = nn.Sequential(
-#             nn.Conv2d(self.num_features // 8, 64, 3, padding=1),
-#             nn.BatchNorm2d(64),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(64, num_classes, 1)
-#         )
-        
-#         self.attention_masks = []
-    
-#     def forward(self, x):
-#         B, C, H, W = x.shape
-#         self.attention_masks = []
-        
-#         x = self.patch_embed(x)
-        
-#         for i_layer in range(self.num_layers):
-#             for block in self.stages[i_layer]:
-                
-#                 # --- GRADIENT CHECKPOINTING MAGIC IS HERE ---
-#                 if self.use_checkpoint and x.requires_grad:
-#                     # We wrap the block call in checkpoint
-#                     x = checkpoint.checkpoint(block, x, use_reentrant=False)
-#                 else:
-#                     x = block(x)
-#                 # --------------------------------------------
-                
-#                 # Collect attention weights
-#                 # Note: With checkpointing, this might require an extra forward pass 
-#                 # or rely on the cached property. For MA-UDA, we usually grab the 
-#                 # weights from the final pass.
-#                 if block.attention_weights is not None:
-#                     self.attention_masks.append(block.attention_weights)
-            
-#             if i_layer < len(self.downsamplers):
-#                 x = self.downsamplers[i_layer](x)
-        
-#         B, N, C = x.shape
-#         H_feat = W_feat = int(N ** 0.5)
-#         x = x.transpose(1, 2).reshape(B, C, H_feat, W_feat)
-        
-#         for dec_layer in self.decoder:
-#             x = dec_layer(x)
-        
-#         output = self.segmentation_head(x)
-        
-#         if output.shape[2:] != (H, W):
-#             output = F.interpolate(output, size=(H, W), mode='bilinear', align_corners=False)
-        
-#         return output
-    
-#     def get_attention_masks(self):
-#         return self.attention_masks
-
-# ... (Imports and previous classes like PatchEmbed, SwinTransformerBlock remain the same) ...
-
 class SwinTransformerSegmentation(nn.Module):
     """
     Swin Transformer-based segmentation model for MA-UDA
-    (Memory Optimized: Only stores necessary attention masks)
     """
     
-    def __init__(self, img_size=128, patch_size=4, in_chans=3, num_classes=4,
-                 embed_dim=48, depths=[2, 2, 6, 2], num_heads=[3, 6, 12, 24],
-                 use_checkpoint=True, window_size=4): 
+    def __init__(self, img_size=256, patch_size=4, in_chans=3, num_classes=4,
+                 embed_dim=96, depths=[2, 2, 6, 2], num_heads=[3, 6, 12, 24],
+                 use_checkpoint=True): # <--- Enabled by default
         super().__init__()
         
         self.use_checkpoint = use_checkpoint
@@ -374,6 +259,7 @@ class SwinTransformerSegmentation(nn.Module):
         
         # Patch embedding
         self.patch_embed = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
+        self.patches_resolution = self.patch_embed.patches_resolution
         
         # Stages and Downsamplers
         self.stages = nn.ModuleList()
@@ -381,11 +267,12 @@ class SwinTransformerSegmentation(nn.Module):
         
         for i_layer in range(self.num_layers):
             layer_dim = int(embed_dim * 2 ** i_layer)
+            
             stage_blocks = nn.ModuleList([
                 SwinTransformerBlock(
                     dim=layer_dim,
                     num_heads=num_heads[i_layer],
-                    window_size=window_size
+                    window_size=4
                 )
                 for _ in range(depths[i_layer])
             ])
@@ -424,33 +311,28 @@ class SwinTransformerSegmentation(nn.Module):
     
     def forward(self, x):
         B, C, H, W = x.shape
-        self.attention_masks = [] # Reset storage
+        self.attention_masks = []
         
         x = self.patch_embed(x)
         
         for i_layer in range(self.num_layers):
             for block in self.stages[i_layer]:
                 
-                # Gradient Checkpointing
+                # --- GRADIENT CHECKPOINTING MAGIC IS HERE ---
                 if self.use_checkpoint and x.requires_grad:
+                    # We wrap the block call in checkpoint
                     x = checkpoint.checkpoint(block, x, use_reentrant=False)
                 else:
                     x = block(x)
+                # --------------------------------------------
                 
-                # === MEMORY FIX STARTS HERE ===
-                # Only save the FIRST attention mask (index 0).
-                # Your trainer code only accesses attn_masks[0].
-                # Saving the others wastes HUGE amounts of VRAM.
-                if len(self.attention_masks) == 0: 
-                    if block.attention_weights is not None:
-                        self.attention_masks.append(block.attention_weights)
-                
-                # Important: If you don't save it, explicitly clear it from the block
-                # to help the garbage collector
-                else:
-                    block.attention_weights = None 
-                # === MEMORY FIX ENDS HERE ===
-
+                # Collect attention weights
+                # Note: With checkpointing, this might require an extra forward pass 
+                # or rely on the cached property. For MA-UDA, we usually grab the 
+                # weights from the final pass.
+                if block.attention_weights is not None:
+                    self.attention_masks.append(block.attention_weights)
+            
             if i_layer < len(self.downsamplers):
                 x = self.downsamplers[i_layer](x)
         
@@ -467,6 +349,124 @@ class SwinTransformerSegmentation(nn.Module):
             output = F.interpolate(output, size=(H, W), mode='bilinear', align_corners=False)
         
         return output
-
+    
     def get_attention_masks(self):
         return self.attention_masks
+
+# ... (Imports and previous classes like PatchEmbed, SwinTransformerBlock remain the same) ...
+
+# class SwinTransformerSegmentation(nn.Module):
+#     """
+#     Swin Transformer-based segmentation model for MA-UDA
+#     (Memory Optimized: Only stores necessary attention masks)
+#     """
+    
+#     def __init__(self, img_size=128, patch_size=4, in_chans=3, num_classes=4,
+#                  embed_dim=48, depths=[2, 2, 6, 2], num_heads=[3, 6, 12, 24],
+#                  use_checkpoint=True, window_size=4): 
+#         super().__init__()
+        
+#         self.use_checkpoint = use_checkpoint
+#         self.num_classes = num_classes
+#         self.num_layers = len(depths)
+#         self.embed_dim = embed_dim
+#         self.num_features = int(embed_dim * 2 ** (self.num_layers - 1))
+        
+#         # Patch embedding
+#         self.patch_embed = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
+        
+#         # Stages and Downsamplers
+#         self.stages = nn.ModuleList()
+#         self.downsamplers = nn.ModuleList()
+        
+#         for i_layer in range(self.num_layers):
+#             layer_dim = int(embed_dim * 2 ** i_layer)
+#             stage_blocks = nn.ModuleList([
+#                 SwinTransformerBlock(
+#                     dim=layer_dim,
+#                     num_heads=num_heads[i_layer],
+#                     window_size=window_size
+#                 )
+#                 for _ in range(depths[i_layer])
+#             ])
+#             self.stages.append(stage_blocks)
+            
+#             if i_layer < self.num_layers - 1:
+#                 self.downsamplers.append(nn.Linear(layer_dim, layer_dim * 2))
+        
+#         # Decoder
+#         self.decoder = nn.ModuleList([
+#             nn.Sequential(
+#                 nn.ConvTranspose2d(self.num_features, self.num_features // 2, 2, stride=2),
+#                 nn.BatchNorm2d(self.num_features // 2),
+#                 nn.ReLU(inplace=True)
+#             ),
+#             nn.Sequential(
+#                 nn.ConvTranspose2d(self.num_features // 2, self.num_features // 4, 2, stride=2),
+#                 nn.BatchNorm2d(self.num_features // 4),
+#                 nn.ReLU(inplace=True)
+#             ),
+#             nn.Sequential(
+#                 nn.ConvTranspose2d(self.num_features // 4, self.num_features // 8, 2, stride=2),
+#                 nn.BatchNorm2d(self.num_features // 8),
+#                 nn.ReLU(inplace=True)
+#             )
+#         ])
+        
+#         self.segmentation_head = nn.Sequential(
+#             nn.Conv2d(self.num_features // 8, 64, 3, padding=1),
+#             nn.BatchNorm2d(64),
+#             nn.ReLU(inplace=True),
+#             nn.Conv2d(64, num_classes, 1)
+#         )
+        
+#         self.attention_masks = []
+    
+#     def forward(self, x):
+#         B, C, H, W = x.shape
+#         self.attention_masks = [] # Reset storage
+        
+#         x = self.patch_embed(x)
+        
+#         for i_layer in range(self.num_layers):
+#             for block in self.stages[i_layer]:
+                
+#                 # Gradient Checkpointing
+#                 if self.use_checkpoint and x.requires_grad:
+#                     x = checkpoint.checkpoint(block, x, use_reentrant=False)
+#                 else:
+#                     x = block(x)
+                
+#                 # === MEMORY FIX STARTS HERE ===
+#                 # Only save the FIRST attention mask (index 0).
+#                 # Your trainer code only accesses attn_masks[0].
+#                 # Saving the others wastes HUGE amounts of VRAM.
+#                 if len(self.attention_masks) == 0: 
+#                     if block.attention_weights is not None:
+#                         self.attention_masks.append(block.attention_weights)
+                
+#                 # Important: If you don't save it, explicitly clear it from the block
+#                 # to help the garbage collector
+#                 else:
+#                     block.attention_weights = None 
+#                 # === MEMORY FIX ENDS HERE ===
+
+#             if i_layer < len(self.downsamplers):
+#                 x = self.downsamplers[i_layer](x)
+        
+#         B, N, C = x.shape
+#         H_feat = W_feat = int(N ** 0.5)
+#         x = x.transpose(1, 2).reshape(B, C, H_feat, W_feat)
+        
+#         for dec_layer in self.decoder:
+#             x = dec_layer(x)
+        
+#         output = self.segmentation_head(x)
+        
+#         if output.shape[2:] != (H, W):
+#             output = F.interpolate(output, size=(H, W), mode='bilinear', align_corners=False)
+        
+#         return output
+
+#     def get_attention_masks(self):
+#         return self.attention_masks
