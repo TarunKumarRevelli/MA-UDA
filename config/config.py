37c6064c11cd453bfac326c033ec1c14a96cf134
tarun-kumar-revelli <cs22b2028@iiitdm.ac.in>
2025-12-26 19:34:46 +0530

    Added dry run provision

--- config/config.py
+++ config/config.py
@@ -44,7 +44,7 @@ class Config:
         
         # Device
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.num_workers = 4
+        self.num_workers = 0
         
         # Visualization
         self.vis_interval = 10
