a960622b5062d41577694973387c6540d2adbce5
tarun-kumar-revelli <cs22b2028@iiitdm.ac.in>
2026-01-05 22:34:24 +0530

    Restarting seg train with cyclegan_99

--- cyclegan_unet.py
+++ cyclegan_unet.py
@@ -80,7 +80,7 @@ class GeneratorResNet(nn.Module):
 # 2. CONFIGURATION
 # ============================================================================
 CONFIG = {
-    'model_path': '/kaggle/working/cyclegan99.pth', 
+    'model_path': '/kaggle/input/cyclegan99/pytorch/default/1/cyclegan_epoch_99.pth', 
     'source_t1_dir': '/kaggle/input/brats19-60-to-90-slices-0-to-3-relabelled/t1',
     'mask_dir': '/kaggle/input/brats19-60-to-90-slices-0-to-3-relabelled/seg',
     'output_img_dir': '/kaggle/working/synthetic_t2_cyclegan',
